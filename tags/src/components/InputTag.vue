83df37e4db959d24867b2f97113b6a129486c3ce
miguellopezpatricio <ml.patri@gmail.com>
2022-10-29 18:07:11 +0200

    Primer proyecto práctica Vue: Tags.

---
+++ tags/src/components/InputTag.vue
@@ -0,0 +1,54 @@
+<script>
+
+export default {
+  data(){
+    return {
+      currentValue:'',
+      tags:[],
+    };
+  },
+
+  methods:{
+    handleKeyDown(e){
+      if(e.key =='Enter' && this.currentValue != ''){
+        this.tags.push(this.currentValue);
+      }
+    },
+
+    handleSubmit() {
+      if (this.currentValue != '') {
+        const exist = this.tags.some((item) => item == this.currentValue);
+
+        if(!exist){
+          this.tags.push(this.currentValue);
+          this.currentValue = '';
+        }
+
+      }
+    },
+
+    deleteTag(tag){
+      this.tags = this.tags.filter(item => item != tag);
+    }
+  },
+};
+</script>
+
+<template>
+
+  <div class="inputTag">
+    <div class="tags">
+      <div class="tag" v-for="(tag, index) in tags" :key="index">
+        {{ tag }} <button @click="deleteTag(tag)">X</button>
+      </div>
+    </div>
+    <form @submit.prevent="handleSubmit">
+      <input type="text" v-model="currentValue" />
+
+
+    </form>
+
+  </div>
+</template>
+
+<style scoped></style>
