528393d220515cd572a3158aab3fb6d0d58c463d
miguellopezpatricio <ml.patri@gmail.com>
2022-10-30 11:49:10 +0100

    Agregando funcionalidad drag & drop

---
+++ drag-drop/src/components/TodosView.vue
@@ -0,0 +1,138 @@
+<script setup>
+
+import {reactive} from "vue";
+import InputNew from "@/components/InputNew.vue";
+
+let boards = reactive([
+    {
+    id: crypto.randomUUID(),
+    name: 'tablero1',
+    items:[
+  {
+    id: crypto.randomUUID(),
+    title: 'Feature de archivos',
+  },
+      {
+        id:crypto.randomUUID(),
+        title: 'Resolver bug',
+      },
+
+],
+},
+
+  {
+    id: crypto.randomUUID(),
+    name: 'tablero2',
+    items: [
+      {
+        id: crypto.randomUUID(),
+        title: 'Mandar reporte',
+      },
+      {
+        id: crypto.randomUUID(),
+        title: 'Code review',
+      },
+    ],
+  },
+]);
+
+function handleNewItem(text,board){
+ // console.log(text.value, board.id, board.name);
+  board.items.push({
+
+
+    id: crypto.randomUUID(),
+    title: text.value,
+  });
+
+}
+
+function handleNewBoard() {
+  const name = prompt("Nombre de nuevo tablero: ");
+
+  if (!!name) {
+    boards.push({
+      id: crypto.randomUUID(),
+      name: name,
+      items: [],
+    });
+  }
+}
+
+function startDrag(event, board, item){
+  event.dataTransfer.setData('text/plain',
+      JSON.stringify({boardId: board.id, itemId: item.id}));
+}
+
+function onDrop(event, destination){
+
+  const{boardId, itemId} = JSON.parse(
+      event.dataTransfer.getData('text/plain'));
+
+  console.log(board.id, item.id);
+
+  const originBoard = boards.find((item) => item.id == boardId);
+
+  const originItem = originBoard.items.find((item) => item.id == itemId);
+
+  destination.items.push({...originItem});
+  originBoard.items =  originBoard.items.filter((item)=> item != originItem);
+
+}
+
+
+
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li><a href="#" @click.prevent="handleNewBoard" >Crear tablero</a> </li>
+    </ul>
+  </nav>
+
+  <div class="boards-container">
+
+    <div class="boards">
+
+    <div class="board"
+         @drop="onDrop($event, board)"
+         @dragover.prevent
+         @dragenter.prevent
+         v-for="board in boards" :key="board.id">
+      <div>{{ board.name }}</div>
+
+      <InputNew  @on-new-item="(text) => handleNewItem(text,board)" />
+      <div class="item" draggable="true" @dragstart="startDrag($event, board, item)" v-for="item in board.items" :key="item.id">
+        {{ item.title }}
+
+      </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.boards{
+  display: flex;
+  gap: 10px;
+}
+
+.board{
+  background-color: #efefef;
+  padding: 10px;
+}
+
+.item{
+  background-color: white;
+  padding: 10px;
+  box-sizing: border-box;
+}
+
+.items{
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+</style>
